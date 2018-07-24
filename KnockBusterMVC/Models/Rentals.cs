2678311eda1f7b545beab38bf8ba78fab8b6039a
edanielbluford <e.daniel.bluford@gmail.com>
2018-07-24 14:27:16 -0400

    Adding more partial metadata views

---
+++ KnockBusterMVC/Models/Rentals.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KnockBusterMVC
+{
+    [MetadataType(typeof(Rentalmetadata))]
+    public partial class Rental
+    {
+    }
+    public class Rentalmetadata
+    {
+        public int RentalID { get; set; }
+        [DisplayName("Customer")]
+        public Nullable<int> CustomerID { get; set; }
+        [DisplayName("Movie")]
+        public Nullable<int> MovieID { get; set; }
+        public Nullable<System.DateTime> RentalDate { get; set; }
+        public System.DateTime DueDate { get; set; }
+        public Nullable<System.DateTime> ReturnDate { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Movy Movy { get; set; }
+    }
+}
